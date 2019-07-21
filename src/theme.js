742ea236d03d9d70e8a58064376829e8c268d39d
Campbell Condiff <campbell@midwesternprint.com>
2019-07-21 13:11:12 -0500

    Initial commit

---
+++ src/theme.js
@@ -0,0 +1,25 @@
+export const typography = {
+  fontFamily: 'sans-serif'
+}
+
+export const color = {
+  primary: '#007afe'
+}
+
+export const lightTheme = {
+  typography,
+  color,
+  textColor: '#000000',
+  backgroundColor: '#ffffff',
+  alternateBackgroundColor: '#f2f1f6',
+  borderColor: '#c4c4c6'
+}
+
+export const darkTheme = {
+  typography,
+  color,
+  textColor: '#ffffff',
+  backgroundColor: '#000000',
+  alternateBackgroundColor: '#1c1c1e',
+  borderColor: '#46454a'
+}
